6e6a39b08814af390834a51ce96717bb45953062
Tahxin <satahsin6@gmail.com>
2026-09-24 18:04:57 +0600

    added hero section

--- src/components/Homepage/Banner.tsx
+++ src/components/Homepage/Banner.tsx
@@ -1,19 +1,34 @@
+
 import React from 'react';
 import Image from 'next/image';
+
 import BannerImage from '@/assets/hero_img.jpg';
 
 const Banner = () => {
-    return (
-        <div className="grid grid-cols-2 items-center gap-4">
-            <div>
-                <h2>Welcome to Book Vibe</h2>
-                <button>Get Started</button>
-            </div>
-            <div>
-                <Image src={BannerImage} alt="Banner Image" width={600} height={400} />
-            </div>
+  return (
+    <section className="py-20">
+      <div className="container mx-auto grid grid-cols-2 items-center gap-4 bg-slate-200 p-3 rounded-4xl shadow-md">
+        <div className="flex flex-col items-center justify-center gap-6">
+          <h2 className="text-3xl font-bold">
+            Books to Freshen Up <br /> your Bookshelf
+          </h2>
+
+          <button className="btn btn-success">
+            View The List
+          </button>
         </div>
-    );
+
+        <div>
+          <Image
+            src={BannerImage}
+            alt="Banner Image"
+            width={600}
+            height={400}
+          />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Banner;
